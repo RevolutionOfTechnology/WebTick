73b70f26ef1626088528575f6386d5eb69c7dba8
Araveli Araujo <Araveli Araujo@ARAVELI>
2017-12-15 00:06:46 -0500

    Sistema de Tickets Online, a trabajar se ha dicho!

---
+++ WebTick/src/java/controller/UserController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+@Named(value = "userController")
+@SessionScoped
+public class UserController implements Serializable {
+
+}
