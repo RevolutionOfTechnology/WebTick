dbf40861c477e7ff68e933be52199f0df252b426
joel <joel@Joel>
2017-12-15 02:47:49 -0500

    da

--- WebTick/src/java/dao/Dao.java
+++ WebTick/src/java/dao/Dao.java
@@ -1,5 +1,43 @@
 package dao;
 
+import java.sql.*;
+
 public class Dao {
 
+    private Connection cn;
+
+    public void Conectar() throws Exception {       //Metodo con los datos de acceso
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            cn = DriverManager.getConnection("jdbc:oracle:thin:@192.168.3.112:1521:XE", "WebTick", "webtick");
+        } catch (ClassNotFoundException | SQLException e) {
+            throw e;
+        }
+    }
+
+    public void Cerrar() throws SQLException {      //Cerrar la coneccion
+        if (cn != null) {
+            if (cn.isClosed() == false) {
+                cn.close();
+            }
+        }
+    }
+
+    public Connection getCn() {
+        return cn;
+    }
+
+    public void setCn(Connection cn) {
+        this.cn = cn;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Dao dao = new Dao();
+        dao.Conectar();
+        if (dao.getCn() != null) {
+            System.out.println("Conectado");
+        } else {
+            System.out.println("error");
+        }
+    }
 }
